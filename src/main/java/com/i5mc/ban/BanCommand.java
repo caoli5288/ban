b1cd1102e19cfc47aa77efad400d8ef0ba2ffb77
caoli5288 <caoli5288@gmail.com>
2017-04-11 23:45:14 +0800

    Fix async kick(called by other plugin async)

--- src/main/java/com/i5mc/ban/BanCommand.java
+++ src/main/java/com/i5mc/ban/BanCommand.java
@@ -1,5 +1,6 @@
 package com.i5mc.ban;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,14 +29,22 @@ public class BanCommand implements CommandExecutor {
 
     private void ban(CommandSender sender, Iterator<String> it) {
         String name = it.next();
-        long expire = it.hasNext() ? new Long(it.next()) * 60000 : plugin.getConfig().getInt("default.expire", 15) * 60000;
+        long expire = it.hasNext() ? Long.parseLong(it.next()) * 60000 : plugin.getConfig().getInt("default.expire", 15) * 60000;
         String reason = it.hasNext() ? it.next() : "";
         Player p = plugin.getPlayer(name);
+        if (Bukkit.isPrimaryThread()) {
+            kick(reason, p);
+        } else {
+            plugin.run(() -> kick(reason, p), 1);
+        }
+        sender.sendMessage("操作已完成");
+        plugin.execute(() -> plugin.ban(name.toLowerCase(), expire, reason));
+    }
+
+    private void kick(String reason, Player p) {
         if (!$.nil(p) && p.isOnline()) {
             p.kickPlayer(reason);
         }
-        plugin.execute(() -> plugin.ban(name.toLowerCase(), expire, reason));
-        sender.sendMessage("操作已完成");
     }
 
 }
